178dc7744f376d6c0dbe2475e5ddae263ca9f2bb
Manjeet Gupta <manjeetgupta6@gmail.com>
2020-07-20 11:50:07 +0530

    spawn signature and 16 bit  bitmask  added

--- 5spawn.go
+++ 5spawn.go
@@ -16,11 +16,12 @@ import (
 	"time"
 )
 
-var redundantDeploymentMap = map[string]uint8{}
+var redundantDeploymentMap = map[string]uint16{}
 var applicaitonpidStateMap = map[string]pidState{}
 var runtimedeploymentMap = map[int]appDetailruntime{}
 var deploymentConfigurationMap map[string][]appDetail
 var ch = make(chan int, 5)
+var chStartNotifier = make(chan bool, 1)
 var wg sync.WaitGroup
 var stateFileName string
 var arg string
@@ -215,7 +216,7 @@ func storeMap(applicaitonpidStateMap map[string]pidState) {
 	Info.Println("<>Leaving storeMap funtion")
 }
 
-func spawnApp(name string, arg string) (chan int, error) {
+func spawnApp(name string, arg string, flag bool) (chan int, error) {
 
 	Info.Println("<>Inside spawnApp funtion")
 	cmd := exec.Command(name, arg)
@@ -236,11 +237,11 @@ func spawnApp(name string, arg string) (chan int, error) {
 
 	// Update the redundantDeploymentMap
 	if v, found := redundantDeploymentMap[name]; found {
-		fmt.Printf("2----%s state before in redundantDeploymentMap :%08b\n", name, v)
+		fmt.Printf("2----%s state before in redundantDeploymentMap :%016b\n", name, v)
 		v = v ^ 1<<lastDigit
 		redundantDeploymentMap[name] = v
 		v, _ := redundantDeploymentMap[name]
-		fmt.Printf("2----%s state after spawning in redundantDeploymentMap :%08b\n", name, v)
+		fmt.Printf("2----%s state after spawning in redundantDeploymentMap :%016b\n", name, v)
 	}
 	updateRedundantDeploymentMap()
 
@@ -496,7 +497,7 @@ func main() {
 	if len(element) > 0 {
 		for _, apptospawn := range element {
 			Info.Println("1----", apptospawn)
-			ch, err = spawnApp(apptospawn.CsciName, apptospawn.ListOfArguments)
+			ch, err = spawnApp(apptospawn.CsciName, apptospawn.ListOfArguments, true)
 			if err != nil {
 				Error.Println("1----Error in spawning", err)
 			}
@@ -511,6 +512,8 @@ func main() {
 		//close(ch)
 	}()
 
+	chStartNotifier <- true
+	//This should become active only after Initialzaton is complete.
 	go fileChangeNotifier()
 
 	//Infinite loop to read from channel
@@ -525,15 +528,15 @@ func main() {
 			argTemp := appDetailruntimeTemp.ListOfArguments
 			delete(runtimedeploymentMap, elem)
 			if v, found := redundantDeploymentMap[nameTemp]; found {
-				fmt.Printf("1----%s state after crash in redundantDeploymentMap :%08b\n", nameTemp, v)
+				fmt.Printf("1----%s state after crash in redundantDeploymentMap :%016b\n", nameTemp, v)
 				v = v ^ 1<<lastDigit
 				redundantDeploymentMap[nameTemp] = v
 				v, _ := redundantDeploymentMap[nameTemp]
-				fmt.Printf("1----%s old state has been reset in redundantDeploymentMap :%08b\n", nameTemp, v)
+				fmt.Printf("1----%s old state has been reset in redundantDeploymentMap :%016b\n", nameTemp, v)
 				updateRedundantDeploymentMap()
 			}
 
-			ch, err = spawnApp(nameTemp, argTemp)
+			ch, err = spawnApp(nameTemp, argTemp, false)
 			if err != nil {
 				Warning.Println("1----Error in spawning", err)
 			}
@@ -542,60 +545,66 @@ func main() {
 }
 
 func fileChangeNotifier() {
+	fmt.Println("<>Inside fileChangeNotifier funtion")
+
 	var mostRecentID int
 	var mostRecentIDstr string
 
-	for {
-		client := &http.Client{}
-		req, err := http.NewRequest("GET", "http://localhost:8384/rest/events?events=LocalChangeDetected", nil)
-		if err != nil {
-			log.Fatal("Error Reading request", err)
-		}
-
-		req.Header.Set("X-API-Key", "crSRLDA6wkz75FuSYoeMtCcdoVi4tsJg")
-		q := req.URL.Query()
-		q.Add("since", mostRecentIDstr)
-
-		req.URL.RawQuery = q.Encode()
-
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal("Error Reading response", err)
-		}
-
-		if resp.Body != nil {
-			defer resp.Body.Close()
-		}
-
-		responseBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal("Error Reading body", err)
-		}
-
-		var responseObject Event
-		json.Unmarshal(responseBody, &responseObject)
-		fmt.Println(string(responseBody))
-		fmt.Println("Length of Response:", len(responseObject))
-
-		if len(responseObject) > 0 {
-			for i := 0; i < len(responseObject); i++ {
-				fmt.Println("ID:", responseObject[i].SubscriptionID)
-				// 	fmt.Println("GlobalID:", responseObject[i].GlobalID)
-				// 	fmt.Println("Time:", responseObject[i].Time)
-				// 	fmt.Println("Type:", responseObject[i].Type)
-				// 	fmt.Println("Data->Action:", responseObject[i].Data.Action)
-				// 	fmt.Println("Data->Folder:", responseObject[i].Data.Folder)
-				// 	fmt.Println("Data->FolderID:", responseObject[i].Data.FolderID)
-				// 	fmt.Println("Data->Label:", responseObject[i].Data.Label)
-				// 	fmt.Println("Data->ModifiedBy:", responseObject[i].Data.ModifiedBy)
-				// 	fmt.Println("Data->Path:", responseObject[i].Data.Path)
-				// 	fmt.Println("Data->Type:", responseObject[i].Data.Type)
+	startFor := <-chStartNotifier
+	fmt.Println("startFor --->", startFor)
+	if startFor {
+		for {
+			client := &http.Client{}
+			req, err := http.NewRequest("GET", "http://localhost:8384/rest/events?events=LocalChangeDetected", nil)
+			if err != nil {
+				log.Fatal("Error Reading request", err)
 			}
-			mostRecentID = responseObject[len(responseObject)-1].SubscriptionID
-			mostRecentIDstr = strconv.Itoa(mostRecentID)
-		}
-		fmt.Println("In for..")
-		time.Sleep(100 * time.Millisecond)
-	}
 
+			req.Header.Set("X-API-Key", "crSRLDA6wkz75FuSYoeMtCcdoVi4tsJg")
+			q := req.URL.Query()
+			q.Add("since", mostRecentIDstr)
+
+			req.URL.RawQuery = q.Encode()
+
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatal("Error Reading response", err)
+			}
+
+			if resp.Body != nil {
+				defer resp.Body.Close()
+			}
+
+			responseBody, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal("Error Reading body", err)
+			}
+
+			var responseObject Event
+			json.Unmarshal(responseBody, &responseObject)
+			fmt.Println(string(responseBody))
+			fmt.Println("Length of Response:", len(responseObject))
+
+			if len(responseObject) > 0 {
+				for i := 0; i < len(responseObject); i++ {
+					fmt.Println("ID:", responseObject[i].SubscriptionID)
+					// 	fmt.Println("GlobalID:", responseObject[i].GlobalID)
+					// 	fmt.Println("Time:", responseObject[i].Time)
+					// 	fmt.Println("Type:", responseObject[i].Type)
+					// 	fmt.Println("Data->Action:", responseObject[i].Data.Action)
+					// 	fmt.Println("Data->Folder:", responseObject[i].Data.Folder)
+					// 	fmt.Println("Data->FolderID:", responseObject[i].Data.FolderID)
+					// 	fmt.Println("Data->Label:", responseObject[i].Data.Label)
+					// 	fmt.Println("Data->ModifiedBy:", responseObject[i].Data.ModifiedBy)
+					// 	fmt.Println("Data->Path:", responseObject[i].Data.Path)
+					// 	fmt.Println("Data->Type:", responseObject[i].Data.Type)
+				}
+				mostRecentID = responseObject[len(responseObject)-1].SubscriptionID
+				mostRecentIDstr = strconv.Itoa(mostRecentID)
+			}
+			fmt.Println("In for..")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+	fmt.Println("<>Leaving fileChangeNotifier funtion")
 }
